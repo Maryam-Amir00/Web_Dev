64485585fd6a6dcc223e92f1d13a5126598609e4
Maryam Amir <herememaryamamir@gmail.com>
2025-01-20 03:55:05 +0500

    Final Commit

--- Day11/file.js
+++ Day11/file.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+let basePrice = 100
+
+const tax = 0.15
+
+let taxAmount = basePrice * tax
+
+let finalPrice = basePrice + taxAmount
+
+console.log(basePrice);
+console.log(tax);
+console.log(finalPrice);
+
+
+
