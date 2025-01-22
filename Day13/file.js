46c1e63da4f2658708b172ba63a9dda91511955b
Maryam Amir <herememaryamamir@gmail.com>
2025-01-23 03:13:26 +0500

    Final Commit

--- Day13/file.js
+++ Day13/file.js
@@ -1,5 +1,16 @@
+console.log("Even Number using For Loop");
+
 for (let i = 1; i <= 20; i++){
-    if (i%2 === 0){
+    if (i % 2 === 0){
         console.log(i);
     }
+}
+
+console.log("Even Number using while Loop");
+let j = 1;
+while (j <= 20){
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+    j++;
 }
