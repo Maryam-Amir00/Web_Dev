fb725337f137f4a73b83939f662624dc99e4e56c
Maryam Amir <herememaryamamir@gmail.com>
2025-01-24 01:49:23 +0500

    Final Commit

--- Day14/file.js
+++ Day14/file.js
@@ -1,2 +1,11 @@
-const Array = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const array1 = [];
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    array1.push(array[i]);
+  }
+}
+
+console.log(array1);
