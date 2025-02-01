70e7ba59c86c7ebdc20ddddfabf28d07fc2c29e3
Maryam Amir <herememaryamamir@gmail.com>
2025-02-02 01:56:18 +0500

    Add Function Added

--- Day17/file.js
+++ Day17/file.js
@@ -9,10 +9,18 @@ function isEven(number) {
     return false;
 }
 
+function add(num1 , num2) {
+    return num1 + num2;
+}
+
 
 const greetings = greet("Maryam")
 console.log(greetings);
 
 const result = isEven(6)
 console.log(result);
+
+const addNumbers = add (2,8)
+console.log(addNumbers);
+
 
