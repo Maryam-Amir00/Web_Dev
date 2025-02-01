524345409e6c11c0d765e00e33901f9fe79c5576
Maryam Amir <herememaryamamir@gmail.com>
2025-02-02 01:54:02 +0500

    isEven Function Added

--- Day17/file.js
+++ Day17/file.js
@@ -2,6 +2,17 @@ function greet(name) {
     return `Hello, ${name}`
 }
 
+function isEven(number) {
+    if(number%2 === 0){
+        return true;
+    }
+    return false;
+}
+
 
 const greetings = greet("Maryam")
 console.log(greetings);
+
+const result = isEven(6)
+console.log(result);
+
