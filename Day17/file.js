85945f53aef4df27dc0ed8dc84c03bc59b4228aa
Maryam Amir <herememaryamamir@gmail.com>
2025-02-02 02:00:59 +0500

    Final Commit

--- Day17/file.js
+++ Day17/file.js
@@ -1,26 +1,34 @@
 function greet(name) {
-    return `Hello, ${name}`
+  return `Hello, ${name}`;
 }
 
 function isEven(number) {
-    if(number%2 === 0){
-        return true;
-    }
-    return false;
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
 }
 
-function add(num1 , num2) {
-    return num1 + num2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
+function maxNumber(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+}
 
-const greetings = greet("Maryam")
+const greetings = greet("Maryam");
 console.log(greetings);
 
-const result = isEven(6)
+const result = isEven(6);
 console.log(result);
 
-const addNumbers = add (2,8)
+const addNumbers = add(2, 8);
 console.log(addNumbers);
 
+const maximum = maxNumber(8 , 4);
+console.log(maximum);
 
