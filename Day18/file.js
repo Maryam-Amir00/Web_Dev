b9de8eec39d542cdb5f2c1f1f3398aaa69f23d92
Maryam Amir <herememaryamamir@gmail.com>
2025-02-03 11:09:02 +0500

    Subtract function added

--- Day18/file.js
+++ Day18/file.js
@@ -5,5 +5,11 @@ function calculator(operation, num1, num2) {
         }
     }
 
+    if (operation === "subtract") {
+        function subtract(param) {
+            return num1 - num2;
+        }
+    }
+
     
 }
