63eec0e27c27ba3d1c0bb4f1cc9e95e9d70cc28b
Maryam Amir <herememaryamamir@gmail.com>
2025-02-03 11:19:44 +0500

    Final Commit

--- Day18/file.js
+++ Day18/file.js
@@ -3,30 +3,41 @@ function calculator(operation, num1, num2) {
         function add() {
             return num1 + num2;
         }
-        add();
+        return add();
     }
 
     if (operation === "subtract") {
         function subtract(param) {
             return num1 - num2;
         }
-        subtract();
+        return subtract();
     }
 
     if (operation === "multiply") {
         function multiply() {
             return num1 * num2;
         }
-        multiply();
+        return multiply();
     }
 
     if (operation === "divide") {
         function divide() {
-            if (num1 === 0) {
-                console.log("Error, Denominator cannot be 0")
+            if (num2 === 0) {
+                console.log("Error: Cannot divide by 0")
                 return;
             }
             return num1 / num2;
         }
+        return divide();
     }
+
+    return "Invalid Operation";
 }
+
+
+console.log(calculator("add", 5, 3));       
+console.log(calculator("subtract", 7, 3));  
+console.log(calculator("multiply", 4, 2)); 
+console.log(calculator("divide", 10, 2));   
+console.log(calculator("divide", 5, 0));    
+console.log(calculator("modulus", 5, 2));   
