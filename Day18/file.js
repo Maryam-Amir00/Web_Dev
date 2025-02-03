86f1c3ce738f81d94c4a53202c2597c3164d2377
Maryam Amir <herememaryamamir@gmail.com>
2025-02-03 11:13:11 +0500

    Calculator added

--- Day18/file.js
+++ Day18/file.js
@@ -3,13 +3,30 @@ function calculator(operation, num1, num2) {
         function add() {
             return num1 + num2;
         }
+        add();
     }
 
     if (operation === "subtract") {
         function subtract(param) {
             return num1 - num2;
         }
+        subtract();
     }
 
-    
+    if (operation === "multiply") {
+        function multiply() {
+            return num1 * num2;
+        }
+        multiply();
+    }
+
+    if (operation === "divide") {
+        function divide() {
+            if (num1 === 0) {
+                console.log("Error, Denominator cannot be 0")
+                return;
+            }
+            return num1 / num2;
+        }
+    }
 }
