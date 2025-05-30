f5662123f6eeafd771b39c258b967fe1f3671ebe
Maryam Amir <herememaryamamir@gmail.com>
2025-05-30 14:06:23 +0500

    Add filter buttons to Todo List for viewing all, active, and completed tasks, enhancing user experience and task management.

--- React/Todo_List/src/App.jsx
+++ React/Todo_List/src/App.jsx
@@ -36,6 +36,39 @@ function App() {
           <div className='backdrop-blur-lg bg-white/30 border border-white/40 shadow-2xl rounded-3xl p-8 w-full max-w-xl '>
             <h1 className='text-4xl font-bold text-center text-purple-800 mb-6 drop-shadow-md'>🌸 TO-DO LIST 🌸</h1>
             <TodoForm  addtodo = {addtodo}/>
+            <div className="flex justify-center gap-2 mb-6">
+  <button
+    onClick={() => setfilter("all")}
+    className={`px-4 py-1 rounded-full text-sm font-medium transition ${
+      filter === "all"
+        ? "bg-purple-500 text-white"
+        : "bg-white/60 hover:bg-white text-purple-600"
+    }`}
+  >
+    All
+  </button>
+  <button
+    onClick={() => setfilter("active")}
+    className={`px-4 py-1 rounded-full text-sm font-medium transition ${
+      filter === "active"
+        ? "bg-purple-500 text-white"
+        : "bg-white/60 hover:bg-white text-purple-600"
+    }`}
+  >
+    Active
+  </button>
+  <button
+    onClick={() => setfilter("completed")}
+    className={`px-4 py-1 rounded-full text-sm font-medium transition ${
+      filter === "completed"
+        ? "bg-purple-500 text-white"
+        : "bg-white/60 hover:bg-white text-purple-600"
+    }`}
+  >
+    Completed
+  </button>
+</div>
+
             <TodoList todos={filteredTodos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
           </div>
         </div>
